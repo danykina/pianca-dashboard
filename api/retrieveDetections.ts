6767fa6f56ddb1b5ab188f5388a4342bdca99b5a
Daniele Chinazzi <daniele@chinazzi.io>
2020-11-27 22:15:59 +0100

    chore: retrive data by controlUnitId, retrive all control units

--- api/retrieveDetections.ts
+++ api/retrieveDetections.ts
@@ -2,15 +2,19 @@ import { FunctionHandler } from './models/function.model';
 
 import axios from 'axios';
 
+const { CIVIL_PROTECTION_BASE_URL } = process.env;
+
 const retrieveDetections: FunctionHandler = async (request, response) => {
+  const { controlUnitId, sensorType = 'all' } = request.query;
+
   try {
     const {
       data: { data },
     } = await axios.get(
-      'https://www.protezionecivile.fvg.it/it/api/drupal/stazione/99/detail/all',
+      `${CIVIL_PROTECTION_BASE_URL}/it/api/drupal/stazione/${controlUnitId}/detail/${sensorType}`,
     );
 
-    return response.status(200).send(data);
+    return response.json(data);
   } catch (error) {
     return response.status(500).send(error);
   }
