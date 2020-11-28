350123acaa0860eedc705f13d7e60c8f8b9b541f
Daniele Chinazzi <daniele@chinazzi.io>
2020-11-28 16:38:35 +0100

    fix: redirect to dashboard route

--- src/containers/core/Router/Router.tsx
+++ src/containers/core/Router/Router.tsx
@@ -24,7 +24,7 @@ const Router: FunctionComponent = () => {
             <Webcams />
           </Route>
 
-          <Redirect to="/home" />
+          <Redirect to="/dashboard" />
         </Switch>
       </NavigationLayout>
     </BrowserRouter>
