e2f835c35cc07964906628099d2fa136974fa86a
Daniele Chinazzi <daniele@chinazzi.io>
2020-11-29 16:35:19 +0100

    chore: service worker for PWA

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,16 @@ import ReactDOM from 'react-dom';
 
 import App from './containers/core/App/App';
 
+import { register } from './serviceWorkerRegistration';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+register();
