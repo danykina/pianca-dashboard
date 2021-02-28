bc988038682dc78c5fac2ed1f4ca4a1264c7456f
Daniele Chinazzi <daniele@chinazzi.io>
2021-02-28 14:15:27 +0100

    Fix: use vercel to redirect on other link

--- src/containers/Webcams/Webcams.tsx
+++ src/containers/Webcams/Webcams.tsx
@@ -6,6 +6,8 @@ import BaseLayout from '../../components/layouts/BaseLayout/BaseLayout';
 
 import AxisWebcam from '../../components/webcams/AxisWebcam/AxisWebcam';
 
+const { REACT_APP_API_BASE_URL } = process.env;
+
 const Webcams: FunctionComponent = () => {
   return (
     <BaseLayout>
@@ -13,7 +15,7 @@ const Webcams: FunctionComponent = () => {
         <Grid item xs={12} sm={12} md={12} lg={6}>
           <AxisWebcam
             cardHeader={{ title: 'Nevelandia' }}
-            src="http://46.19.234.136/axis-cgi/mjpg/video.cgi"
+            src={`${REACT_APP_API_BASE_URL}/axis-cgi/mjpg/video.cgi`}
             alt="Webcam Nevelandia"
           />
         </Grid>
